c15f78790098e3eb9e2ad6c3e490c7b59dafbd3f
Daniel Tsui <tsui.daniel@gmail.com>
2018-12-06 16:49:13 +1100

    new build for github pages

---
+++ docs/precache-manifest.96526249060d9c9bed26e6bc624415cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b29bd1a451cbf43d535",
+    "url": "/react-emoji-code-search/static/js/runtime~main.9b29bd1a.js"
+  },
+  {
+    "revision": "842a21f6e824d19dd2e8",
+    "url": "/react-emoji-code-search/static/js/main.842a21f6.chunk.js"
+  },
+  {
+    "revision": "27cd81d94bff8af28805",
+    "url": "/react-emoji-code-search/static/js/1.27cd81d9.chunk.js"
+  },
+  {
+    "revision": "842a21f6e824d19dd2e8",
+    "url": "/react-emoji-code-search/static/css/main.48a07725.chunk.css"
+  },
+  {
+    "revision": "acecca23304b514c47c370012ec413d3",
+    "url": "/react-emoji-code-search/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65351ce730ffafffaf881e373aac92ab.js"
+  "/react-emoji-code-search/precache-manifest.96526249060d9c9bed26e6bc624415cb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-emoji-code-search/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
