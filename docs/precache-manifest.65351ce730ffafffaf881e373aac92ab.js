a7a3acbbf8275d731b9a899a791ab8d0e2ba068f
Daniel Tsui <tsui.daniel@gmail.com>
2018-12-06 15:49:19 +1100

    build for experimental deploy

---
+++ docs/precache-manifest.65351ce730ffafffaf881e373aac92ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d7063b28b05ae3f28298",
+    "url": "/static/js/main.d7063b28.chunk.js"
+  },
+  {
+    "revision": "27cd81d94bff8af28805",
+    "url": "/static/js/1.27cd81d9.chunk.js"
+  },
+  {
+    "revision": "d7063b28b05ae3f28298",
+    "url": "/static/css/main.48a07725.chunk.css"
+  },
+  {
+    "revision": "a77d4a040ae951353439ae6a23df5c58",
+    "url": "/index.html"
+  }
+];
