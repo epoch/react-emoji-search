358a6e3348191bcd21e959436898ee6b44e24191
Daniel Tsui <tsui.daniel@gmail.com>
2019-05-23 16:13:39 +1000

    Updates

---
+++ precache-manifest.509d6ae2f6d5123ebb68cf3ec50e2d05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61df7af9f2396246e2a7",
+    "url": "/react-emoji-search/static/js/runtime~main.61df7af9.js"
+  },
+  {
+    "revision": "dd7646336b1738c954a8",
+    "url": "/react-emoji-search/static/js/main.dd764633.chunk.js"
+  },
+  {
+    "revision": "e546c748222bc311762c",
+    "url": "/react-emoji-search/static/js/1.e546c748.chunk.js"
+  },
+  {
+    "revision": "dd7646336b1738c954a8",
+    "url": "/react-emoji-search/static/css/main.a2e613ed.chunk.css"
+  },
+  {
+    "revision": "03072dcd238bf4494a073eed31a8c6a7",
+    "url": "/react-emoji-search/index.html"
+  }
+];
